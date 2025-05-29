d1da7f9d58132a65374450c67ca984fcdfafff3e
RuslanRuslan89 <ruslankon89@gmail.com>
2025-05-29 19:57:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -49,3 +49,27 @@ window.addEventListener('scroll', function () {
         document.body.classList.remove('scrolled');
     }
 });
+// Подсветка активного пункта меню
+document.addEventListener('DOMContentLoaded', function () {
+    const sections = document.querySelectorAll("section[id]");
+    const navLinks = document.querySelectorAll(".nav a");
+
+    window.addEventListener('scroll', () => {
+        let current = '';
+
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop - 100;
+            const sectionHeight = section.offsetHeight;
+            if (pageYOffset >= sectionTop && pageYOffset < sectionTop + sectionHeight) {
+                current = section.getAttribute("id");
+            }
+        });
+
+        navLinks.forEach(link => {
+            link.classList.remove("active");
+            if (link.getAttribute("href") === "#" + current) {
+                link.classList.add("active");
+            }
+        });
+    });
+});
