b98b3db5d8538ab4350a70edd8ca8179d3e1ad2b
RuslanRuslan89 <ruslankon89@gmail.com>
2025-05-29 20:01:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -73,3 +73,17 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+// Кнопка "Наверх"
+const backToTopButton = document.getElementById('backToTop');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        backToTopButton.style.display = 'block';
+    } else {
+        backToTopButton.style.display = 'none';
+    }
+});
+
+backToTopButton.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
