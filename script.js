0b824b707567f1c202eea06fb29f726ba9080856
RuslanRuslan89 <ruslankon89@gmail.com>
2025-05-29 19:52:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -40,3 +40,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     sections.forEach(section => observer.observe(section));
 });
+
+// Эффект затухания при скролле
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 50) {
+        document.body.classList.add('scrolled');
+    } else {
+        document.body.classList.remove('scrolled');
+    }
+});
